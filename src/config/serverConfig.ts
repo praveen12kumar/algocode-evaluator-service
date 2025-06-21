4616c73b6eb68cb5c838e205c0f2b7038d239332
Praveen Kumar <praveenshakya441@gmail.com>
2025-06-21 22:14:23 +0530

    Feat: Basic ioredis setup with bullmq installation done

--- src/config/serverConfig.ts
+++ src/config/serverConfig.ts
@@ -3,5 +3,7 @@ import { config } from "dotenv";
 config();
 
 export default {
-    PORT: process.env.PORT || 3000
+    PORT: process.env.PORT || 3000,
+    REDIS_PORT: parseInt(process.env.REDIS_PORT || '6379'),
+    REDIS_HOST: process.env.REDIS_HOST  || '127.0.0.1'
 };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express,{ Express} from 'express';
 
 import serverConfig from './config/serverConfig';
+import apiRouter from './routes';
 
 const app: Express = express();
+
+app.use('/api', apiRouter);
 
 
 
 app.listen(serverConfig.PORT, ()=>{
+    // eslint-disable-next-line no-console
     console.log(`Server is listening on port ${serverConfig.PORT}`);
 });
