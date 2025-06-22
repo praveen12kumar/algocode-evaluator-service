d4d384fbba5d2f6321d08c4b17ac5625aac45c2a
Praveen Kumar <praveenshakya441@gmail.com>
2025-06-22 22:26:03 +0530

    Feat: Added bullMQ ui Dashboard

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 import express,{ Express} from 'express';
 
+import bullBoardAdapter from './config/bullBoardConfig';
 import serverConfig from './config/serverConfig';
 import sampleQueueProducer from './producers/sampleQueueProducer';
 import apiRouter from './routes';
 import { SampleWorker } from './workers/sampleWorker';
 
+
 const app: Express = express();
 
 app.use('/api', apiRouter);
-
+app.use('/ui', bullBoardAdapter.getRouter());
 
 
 app.listen(serverConfig.PORT, ()=>{
